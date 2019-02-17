f080d1ca2615c1c8eaa14c7a62c5a88609536372
James Cho <hwisoocho@gmail.com>
2019-02-16 21:34:40 -0800

    Stopwatch state

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
 
 class Stopwatch extends Component {
+  state = {
+    isRunning: false
+  };
+
+  handleStopWatch = () => {
+    this.setState({
+      isRunning: !this.state.isRunning
+    });
+  };
+
   render() {
     return (
       <div className="stopwatch">
         <h2>Stopwatch</h2>
         <span className="stopwatch-time">0</span>
-        <button>Start</button>
+        <button onClick={this.handleStopWatch}>
+          {this.state.isRunning ? "Stop" : "Start"}
+        </button>
         <button>Reset</button>
       </div>
     );
