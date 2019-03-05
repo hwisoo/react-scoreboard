f271ae18d3dfc0f8c675b4e122d24bf33f3fd670
James Cho <hwisoocho@gmail.com>
2019-03-05 11:11:27 -0800

    intro to Reducers

---
+++ src/reducers/player.js
@@ -0,0 +1,66 @@
+import * as PlayerActionTypes from '../actiontypes/player';
+
+const initialState =    [
+    {
+      name: "Leonardo",
+      score: 0,
+      id: 1
+    },
+    {
+      name: "Michelangelo",
+      score: 0,
+      id: 2
+    },
+    {
+      name: "Donatello",
+      score: 0,
+      id: 3
+    },
+    {
+      name: "Raphael",
+      score: 0,
+      id: 4
+    }
+  ]
+
+export default function Player(state=initialState, action) {
+  switch(action.type) {
+    case PlayerActionTypes.ADD_PLAYER:
+      return [
+        ...state,
+        {
+          name: action.name,
+          score: 0
+        }
+      ];
+      
+    case PlayerActionTypes.REMOVE_PLAYER:
+      return [
+        ...state.slice(0, action.index),
+        ...state.slice(action.index + 1)
+      ];
+      
+    case PlayerActionTypes.UPDATE_PLAYER_SCORE:
+      return state.map((player, index) => {
+        if(index === action.index) {
+          return {
+            ...player,
+            score: player.score + action.score
+          };
+        }
+        return player;
+      });
+      
+    default:
+      return state;
+  }
+}
+
+
+
+
+
+
+
+
+
