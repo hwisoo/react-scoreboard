345764e078b33838fecb4b146f299eafea47ae86
James Cho <hwisoocho@gmail.com>
2019-03-05 11:39:21 -0800

    creating a redux store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import {render} from "react-dom";
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+import PlayerReducer from './reducers/player';
 import Scoreboard from "./containers/Scoreboard";
 import "./index.css";
 
-ReactDOM.render(<Scoreboard />, document.getElementById("root"));
+const store = createStore(PlayerReducer);
+
+render(
+<Provider store={store}>
+<Scoreboard />
+</Provider>, 
+document.getElementById("root"));
